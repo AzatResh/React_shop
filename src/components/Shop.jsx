25f17d16d861d88219722511516f3add17a10016
AzatResh <Azat.R.2003@mail.ru>
2023-07-11 21:36:27 +0600

    - fetch function

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import {API_KEY, API_URL} from '../config';
 
 function Shop(){
+
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    // get Products from api
+    useEffect(()=>{
+        fetch(API_URL, {
+            headers:{ 
+                'Authorization': API_KEY,
+            },
+        })
+        .then(resp => resp.json())
+        .then((data) => {
+            data.shop && setProducts(data.shop);
+            setLoading(false);
+        })
+    }, [])
+
     return(
         <main className="container content">
 
             Shop
-            
+
         </main>
     )
 }
